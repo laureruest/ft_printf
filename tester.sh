7a98a9560fe9ad93563f26e7477c257a80e7279b
lruiz-es <lruiz-es@student.42barcelona.com>
2024-03-03 09:14:16 +0100

    Empezando archivos de pruebas

---
+++ tester.sh
@@ -0,0 +1,36 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    tester.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lruiz-es <lruiz-es@student.42barcel>       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/03/03 08:41:31 by lruiz-es          #+#    #+#              #
+#    Updated: 2024/03/03 09:12:30 by lruiz-es         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+make re
+cc -g -Wall -Werror -Wextra main.c libftprintf.a libft/libft.a
+mv a.out x_to_leaks
+cc -g -fsanitize=address -Wall -Werror -Wextra main.c libftprintf.a libft/libft.a
+echo "++++++++++++++Informe de ejecucion++++++++++++++++++++++++++"
+echo "Se imprimiran los errores de gestion de memoria, si los hubiere"
+./a.out > user_output/output
+echo 
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+++++++++Informe de errores de diferencias al+++++++++++++++"
+echo "+++++++++++++comportamiento esperado++++++++++++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+diff test_output_files/output user_output/output >> log.txt
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++++++++Se imprimira informacion de LEAKS+++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+leaks -atExit ./x_to_leaks
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo ++++++++++++++++++FIN DEL INFORME DE EJECUCION+++++++++++++++

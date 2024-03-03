8d2a36a408db9903524dd87e72f068209504c64a
lruiz-es <lruiz-es@student.42barcelona.com>
2024-03-03 12:26:56 +0100

    Creciendo las pruebas

---
+++ gentestfile.sh
@@ -0,0 +1,2 @@
+cc  -Wall -Werror -Wextra main.c
+./a.out > test_output_files/output
---
+++ main.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lruiz-es <lruiz-es@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 10:03:35 by lruiz-es          #+#    #+#             */
+/*   Updated: 2024/03/03 12:26:34 by lruiz-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Comment & Uncomment where necessary
+#include <stdio.h>
+//#include "libftprintf.h"
+
+int	main(void)
+{
+	// starting with data for generator and for tests
+	char	battery_test_strings[5][3] = {
+		{"\0Esto es una cadena de formato NULL%s", "Esto es un string que\
+		no se imprimira", "Esto tampoco"};
+		{"En esta prueba imprimimos un caracter S: %c Adios\0 y a partir \
+			de aqui nada mas", "Se imprimio	solo la \0primera s", ""};
+		{"En esta prueba imprimimos un cadena \34Se imprimio solo la \34, el re\
+		   sto se ignora:\34%s\34 Adios\0 y a partir de aqui nada mas", \
+			   "Se imprimio	solo la \0primera s", "Esta ni entrara a printf"};
+		{"En esta prueba imprimimos el primer caracter de la cadena \34Se impr\
+			imio solo la \34 que por lo tanto es la S: %c\nY ahora la cadena\
+				completa \34Se imprimio solo la \34: %s", "Se imprimio solo \
+				la ", "Se imprimio solo la "};
+		{"En esta prueba imprimimos el signo \34\37\34: %%. Y ahora el primer\
+		   	caracter de la cadena \34Se imprimio solo la \34, que por lo tanto\
+			   	es la S: %c\nY ahora la cadena completa \34Se imprimio solo la \
+				34: %s", "Se imprimio solo la ", "Se imprimio solo la \0y esto\
+			   	no se imprime"};
+	};
+	char	for_nb_format_ptr = 
+		{"Ahora imprimimos la direccion de un puntero: %p\0que deberia imprimir \
+			el puntero en hexadecimal: %p\0 y nada de esto"};
+	void	*ptr[8];
+	int	i;
+	int nreturned;
+
+	ptr[0] = (void *) 0;
+	ptr[1] = (void *) 255;
+	ptr[2] = (void *) 65535;
+	ptr[3] = (void *) 167777215;
+	ptr[4] = (void *) 4294967295;
+	ptr[5] = (void *) 1099511627775;
+	ptr[6] = (void *) 281474976710655;
+	ptr[7] = (void *) 281474976710654;
+	for (i = 0, i < 5, i++)
+	{
+		nreturned = printf(&battery_test_strings[i][0], &battery_test_strings\
+				[i][1], &battery_test_strings[2]);
+	//nreturned = ft_printf(&battery_test_strings[i][0], &battery_test_strings\
+				[i][1], &battery_test_strings[2]);
+	}
+	nreturned = printf("Imprimimos un monton de caracteres iniciales de cuatro \
+			cadenas diferentes %c %c %c y un string %s", "Ahora la A", "Bamos \
+			con la B", "Continuamos con la C", "Esto es la cadena completa");
+//	nreturned = ft_printf("Imprimimos un monton de caracteres iniciales de \
+			cuatro cadenas diferentes %c %c %c y un string %s", "Ahora la a", \
+			"Bamos con la B", "Continuamos con la C", "Esto es la cadena com\
+			pleta");
+	for (i = 0, i < 7, i += 2)
+	{
+		nreturned = printf("Imprimo el puntero numero %i: %p\n", i, p[i], i + 1\
+				, p[i + 1]);
+	//	nreturned = ft_printf("Imprimo el puntero numero %i: %p\n", i, p[i]\
+				, i + 1, p[i + 1]);
+	}
+
+
+
+
+}

372b4c7348d5cc762ad0640a00c95be14936d70c
lruiz-es <lruiz-es@student.42barcelona.com>
2024-03-10 09:40:54 +0100

    Firsts lines in ft_printf and instructions & ended ft_printf.h

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -3,17 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lruiz-es <lruiz-es@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: lruiz-es <lruiz-es@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/09 20:16:54 by lruiz-es          #+#    #+#             */
-/*   Updated: 2024/03/09 20:42:44 by lruiz-es         ###   ########.fr       */
+/*   Created: 2024/03/10 08:01:02 by lruiz-es          #+#    #+#             */
+/*   Updated: 2024/03/10 09:38:48 by lruiz-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include "libft.h"
+#include <stdlib.h>
 
 int	ft_printf(const char *, ...)
 {
-	
+	size_t	fidx;
+	size_t	idx;
 }

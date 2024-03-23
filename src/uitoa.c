b6349863017f3ff99d835e440d0e976201d33f60
Laureano Salvador Ruiz Estévez <laureruest@gmail.com>
2024-03-23 16:12:49 +0100

    Looks working for evaluation

--- src/uitoa.c
+++ src/uitoa.c
@@ -6,13 +6,13 @@
 /*   By: lruiz-es <lruiz-es@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 14:03:28 by lruiz-es          #+#    #+#             */
-/*   Updated: 2024/03/23 14:14:17 by lruiz-es         ###   ########.fr       */
+/*   Updated: 2024/03/23 15:51:55 by lruiz-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static int	my_pow(int b, unsigned short int exp)
+static unsigned long long int	my_pow(unsigned int b, unsigned short int exp)
 {
 	if (exp == 0)
 		return (1);
@@ -22,40 +22,34 @@ static int	my_pow(int b, unsigned short int exp)
 	return (b * my_pow(b, exp));
 }
 
-static char	conv_dig(int n, unsigned short int wg)
+static char	conv_dig(unsigned long long int n, unsigned short int wg)
 {
-	int	dig;
+	unsigned long long int	dig;
 
 	dig = n / (my_pow(10, wg));
-	if (dig < 0)
-		dig *= -1;
-	return (((unsigned char) dig) + '0' - '\0');
+	dig += '0' - '\0';
+	return (dig);
 }
 
 // redim ptr data size & fill string
-static void	ins(char *ptr, int n, size_t cursor, unsigned short int exp)
+static void	ins(char *p, unsigned int n, size_t cur, unsigned short int exp)
 {
-	if ((n < 0) && (cursor == 0))
-	{
-		ptr[cursor] = '-';
-		cursor ++;
-	}
-	ptr[cursor] = conv_dig(n, exp);
-	cursor++;
+	p[cur] = conv_dig(n, exp);
+	cur++;
 	if (exp == 0)
 	{
-		ptr[cursor] = '\0';
+		p[cur] = '\0';
 		return ;
 	}
 	n %= my_pow(10, exp);
 	exp--;
-	ins(ptr, n, cursor, exp);
+	ins(p, n, cur, exp);
 }
 
 // compute the more significave's exponent for base_10 of ABS(n)
-static unsigned short int	c_wg(int n, unsigned short int exp)
+static unsigned int	c_wg(unsigned int n, unsigned short int exp)
 {
-	if ((n > -10) && (n < 10))
+	if (n < 10)
 		return (exp);
 	exp++;
 	return (c_wg(n / 10, exp));
@@ -63,7 +57,7 @@ static unsigned short int	c_wg(int n, unsigned short int exp)
 
 // put sing, count the size of work (calculatin wg, weight of digit more sig)
 // and starts
-char	*ft_itoa(unsigned int n)
+char	*uitoa(unsigned int n)
 {
 	unsigned short int	wg;
 	char				*tor;
@@ -71,8 +65,6 @@ char	*ft_itoa(unsigned int n)
 
 	wg = c_wg(n, 0);
 	l = ((size_t) wg) + 2;
-	if (n < 0)
-		l++;
 	tor = malloc(l);
 	if (tor != NULL)
 	{
